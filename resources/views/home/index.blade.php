252bd8bf0ec0746d566243c606d9cbd37b885779
陳泓維 <tim5556618@gmail.com>
2017-10-31 22:13:55 +0800

    建立子樣板

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.master')
+        @section('tittle','Home')
+        @section('content')
+            <h1>Home</h1>
+            <p>This is my body content.</p>
+            @endsection
